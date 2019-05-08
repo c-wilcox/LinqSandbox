cdf7bdb12ff2a0b6bea466194185ca560c0072ff
craig wilcox <serverside6@gmail.com>
2019-05-08 13:52:39 -0400

    Hello Linq!  A little data, a little LINQ a little Console.WriteLine() .

---
+++ DataSources/DataSourceBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+
+
+namespace DataSources
+{
+    public static class DataSourceBuilder
+    {
+        public static IEnumerable<TestDataItem> GetTestDataItems()
+        {
+            var items = new List<TestDataItem>();
+
+            for (int i = 1; i < 2000; i++)
+            {
+                var item = new TestDataItem
+                {
+                    DataItemId = i
+                };
+
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+
+    public class TestDataItem
+    {
+        public int DataItemId { get; set; }
+    }
+}
--- MainConsole/Program.cs
+++ MainConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataSources;
 
 namespace MainConsole
 {
@@ -7,6 +10,15 @@ namespace MainConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            IEnumerable<TestDataItem> dataItems = DataSourceBuilder.GetTestDataItems();
+
+            var query = dataItems.Where(i => i.DataItemId > 1900);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.DataItemId);
+            }
+
             Console.ReadLine();
         }
     }

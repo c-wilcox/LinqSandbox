3f34493e1ab4619f93b179460081ba3e819098d9
craig wilcox <serverside6@gmail.com>
2019-05-21 20:33:12 -0400

    Loading Waypoints, yay! Now time to pick and choose child nodes and generate CSV!

---
+++ GeocachingGPXProcessor/GpxLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Xml.Linq;
+
+namespace GeocachingGPXProcessor
+{
+    public static class GpxLoader
+    {
+        public static IEnumerable<XElement> LoadWaypoints(string fullPath)
+        {
+            XDocument doc = XDocument.Load(fullPath);
+
+            var mainNode = (XElement)doc.FirstNode;
+            IEnumerable<XElement> wayPoints = from node in mainNode.Nodes()
+                where ((XElement) node).Name.LocalName.Equals("wpt")
+                select (XElement)node;
+
+            return wayPoints;
+        }
+        
+    }
+}
---
+++ GeocachingGPXProcessor/MainConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace GeocachingGPXProcessor
+{
+    class MainConsole
+    {
+        static void Main()
+        {
+            Console.WriteLine("Loading Document...");
+
+            IEnumerable<XElement> gpxDoc = GpxLoader.LoadWaypoints(@"C:\Users\serverside6\Documents\My Source Code\LinqSandbox\GeocachingGPXProcessor\GPX Files\List of My HIdes.gpx");
+
+            foreach (XElement n in gpxDoc)
+            {
+                Console.WriteLine($"{n}\r\n=================================");
+            }
+
+            Console.WriteLine("Document Loaded!");
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
